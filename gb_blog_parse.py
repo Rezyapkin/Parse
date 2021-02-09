addeb2c7ba3b2eb60050a58adf05d8e46911049e
Rezyapkin <rezyapkin@yandex.ru>
2021-02-09 10:03:16 +0300

    DRY code

--- gb_blog_parse.py
+++ gb_blog_parse.py
@@ -11,12 +11,27 @@ from database import Database
 # todo Извлечь данные: Url, Заголовок, имя автора, url автора, список тегов (url, имя)
 
 
+# В исходной версии есть нарушение принципа DRY в коде, который ставит задачи
+
+class Task:
+
+    def __init__(self):
+        self.tasks = []
+        self.done_urls = set()
+
+    def add(self, url, parse_func):
+        if url not in self.done_urls:
+            self.tasks.append(parse_func)
+            self.done_urls.add(url)
+
+
 class GbParse:
+
     def __init__(self, start_url, database):
         self.start_url = start_url
         self.done_urls = set()
-        self.tasks = [self.parse_task(self.start_url, self.pag_parse)]
-        self.done_urls.add(self.start_url)
+        self.task = Task()
+        self.task.add(self.start_url, self.parse_task(self.start_url, self.pag_parse))
         self.database = database
 
     def _get_soup(self, *args, **kwargs):
@@ -32,10 +47,11 @@ class GbParse:
         return wrap
 
     def run(self):
-        for task in self.tasks:
+        for task in self.task.tasks:
             result = task()
             if result:
-                self.database.create_post(result)
+                print(result)
+                # self.database.create_post(result)
 
     def post_parse(self, url, soup: bs4.BeautifulSoup) -> dict:
         author_name_tag = soup.find("div", attrs={"itemprop": "author"})
@@ -61,20 +77,15 @@ class GbParse:
     def pag_parse(self, url, soup: bs4.BeautifulSoup):
         gb_pagination = soup.find("ul", attrs={"class": "gb__pagination"})
         a_tags = gb_pagination.find_all("a")
+
         for a in a_tags:
             pag_url = urljoin(url, a.get("href"))
-            if pag_url not in self.done_urls:
-                task = self.parse_task(pag_url, self.pag_parse)
-                self.tasks.append(task)
-                self.done_urls.add(pag_url)
+            self.task.add(pag_url, self.parse_task(pag_url, self.pag_parse))
 
         posts_urls = soup.find_all("a", attrs={"class": "post-item__title"})
         for post_url in posts_urls:
             post_href = urljoin(url, post_url.get("href"))
-            if post_href not in self.done_urls:
-                task = self.parse_task(post_href, self.post_parse)
-                self.tasks.append(task)
-                self.done_urls.add(post_href)
+            self.task.add(post_href, self.parse_task(post_href, self.post_parse))
 
 
 if __name__ == "__main__":
