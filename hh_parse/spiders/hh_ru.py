a97e7e7e0c224c22074c979f4911274acee96ed5
Rezyapkin <rezyapkin@yandex.ru>
2021-02-11 10:39:07 +0300

    start scrapy project

---
+++ hh_parse/spiders/hh_ru.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class HhRuSpider(scrapy.Spider):
+    name = 'hh_ru'
+    allowed_domains = ['hh.ru']
+    start_urls = ['https://hh.ru/search/vacancy?schedule=remote&L_profession_id=0&area=113']
+
+    def parse(self, response):
+        pass
